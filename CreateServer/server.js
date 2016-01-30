0a54e4b86d1915eef5809aa5e09e3e769b0af463
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-30 21:58:19 +0100

    Basic server creation

---
+++ CreateServer/server.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var url = require("url");
+
+var server = http.createServer(function(req, res)
+{
+	var page = url.parse(req.url).pathname;
+	if (page == "/")
+	{
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(
+			'<!DOCTYPE html>'+
+			'<html>'+
+			'	<head>'+
+			'		<meta charset="utf-8" />'+
+			'		<title>Yo</title>'+
+			'	</head>'+
+			'	<body>'+
+			'		<p>Yo !</p>'+
+			'	</body>'+
+			'</html>');
+		res.end();
+	}
+	else
+	{
+		res.writeHead(404, {"Content-Type": "text/html"});
+		res.write(
+			'<!DOCTYPE html>'+
+			'<html>'+
+			'	<head>'+
+			'		<meta charset="utf-8" />'+
+			'		<title>Yo</title>'+
+			'	</head>'+
+			'	<body>'+
+			'		<p>404 not found !</p>'+
+			'	</body>'+
+			'</html>');
+		res.end();
+	}
+});
+server.listen(8080);
